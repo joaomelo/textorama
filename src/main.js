f6492d029fe8001861576fad3bef265ec29f6e4e
joão melo <j@melo.plus>
2022-11-17 19:49:34 -0300

    feat: manual deploy to hosting

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { createApp } from "vue";
 import { createVuestic } from "vuestic-ui";
 import "vuestic-ui/css";
@@ -8,6 +10,18 @@ import App from "./app.vue";
 import { TextRecord } from "./text-record";
 
 export function initApp(elementId) {
+  const firebaseConfig = {
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  };
+  const firebase = initializeApp(firebaseConfig);
+  getAnalytics(firebase);
+
   const app = createApp(App);
 
   const textRecord = new TextRecord(welcome);
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import md from "vite-plugin-markdown";
 
 export default defineConfig({
   plugins: [vue()],
