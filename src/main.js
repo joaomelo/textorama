07545500837930a066b59c74b60f8e7761558705
joão melo <j@melo.plus>
2022-11-20 14:19:41 -0300

    fix: sentry release references bumped package.json version

--- src/main.js
+++ src/main.js
@@ -17,22 +17,23 @@ import App from "./app.vue";
 import { TextRecord } from "./text-record";
 
 export function initApp(elementId) {
-  const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_APP_ID,
-    measurementId: import.meta.env.VITE_MEASUREMENT_ID,
-  };
-  const firebase = initializeApp(firebaseConfig);
-  getAnalytics(firebase);
-
   const app = createApp(App);
 
   app.provide("version", version);
+
   if (import.meta.env.PROD) {
+    const firebaseConfig = {
+      apiKey: import.meta.env.VITE_API_KEY,
+      authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+      projectId: import.meta.env.VITE_PROJECT_ID,
+      storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+      messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+      appId: import.meta.env.VITE_APP_ID,
+      measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+    };
+    const firebase = initializeApp(firebaseConfig);
+    getAnalytics(firebase);
+
     Sentry.init({
       release: version,
       app,
