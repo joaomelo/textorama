7efd4f203fbf18c2de435dbf570245267285c721
joão melo <j@melo.plus>
2022-11-25 18:28:33 -0300

    tests: poc e2e testing

---
+++ cypress.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  fixturesFolder: "e2e/fixtures",
+  screenshotsFolder: "e2e/screenshots",
+  videosFolder: "e2e/videos",
+  downloadsFolder: "e2e/downloads",
+  e2e: {
+    baseUrl: "http://127.0.0.1:5173",
+    supportFile: "e2e/support/e2e.js",
+    specPattern: "e2e/**/*.spec.js",
+    experimentalSessionAndOrigin: true,
+    experimentalRunAllSpecs: true,
+  },
+});
---
+++ e2e/plain.spec.js
@@ -0,0 +1,5 @@
+describe("basic e2e tests", () => {
+  it("should visit", () => {
+    cy.visit("/");
+  });
+});
