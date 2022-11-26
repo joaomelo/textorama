a5777434d78967119f0f4be56b53f0c2e1b9c3e1
joão melo <j@melo.plus>
2022-11-26 10:31:38 -0300

    tests: basic integration with cypress cloud

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "k3zc22",
   fixturesFolder: "e2e/fixtures",
   screenshotsFolder: "e2e/screenshots",
   videosFolder: "e2e/videos",
