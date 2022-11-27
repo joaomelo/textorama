bd25325160afff50ff9b95eb93b798577327b173
joão melo <j@melo.plus>
2022-11-27 15:51:49 -0300

    feat: better pwa support with vite-plugin-pwa e realfavicongenerator

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,94 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import vue from "@vitejs/plugin-vue";
 
-export default defineConfig({
-  plugins: [vue()],
-  root: "src",
-  envDir: "../",
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-    sourcemap: true,
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      vue(),
+      VitePWA({
+        registerType: "autoUpdate",
+        injectRegister: "auto",
+        workbox: {
+          globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        },
+        devOptions: {
+          enabled: true,
+        },
+        includeAssets: [
+          "favicon.ico",
+          "apple-touch-icon.png",
+          "safari-pinned-tab.svg",
+        ],
+        manifest: createManifest(mode),
+      }),
+    ],
+    root: "src",
+    envDir: "../",
+    publicDir: "../assets",
+    build: {
+      outDir: "../dist",
+      assetsDir: ".",
+      emptyOutDir: true,
+      sourcemap: true,
+    },
+  };
 });
+
+function createManifest(mode) {
+  const name = mode === "development" ? "plain (dev)" : "plain";
+  const android192 = {
+    src: "/android-chrome-192x192.png",
+    sizes: "192x192",
+    type: "image/png",
+  };
+  const android512 = {
+    src: "/android-chrome-512x512.png",
+    sizes: "512x512",
+    type: "image/png",
+  };
+  return {
+    name,
+    short_name: name,
+    description:
+      "plain is a modest text editor. It works with plain text files in your file system",
+    categories: ["productivity", "utilities"],
+    start_url: "/",
+    id: "/",
+    theme_color: "#154EC1",
+    background_color: "#ffffff",
+    display: "standalone",
+    lang: "en",
+    orientation: "portrait",
+    icons: [
+      android192,
+      android512,
+      {
+        ...android512,
+        purpose: "maskable",
+      },
+    ],
+    screenshots: [
+      {
+        src: "screenshot-1.png",
+        sizes: "464x821",
+        type: "image/png",
+      },
+      {
+        src: "screenshot-2.png",
+        sizes: "1310x932",
+        type: "image/png",
+      },
+    ],
+    file_handlers: [
+      {
+        action: "/",
+        launch_type: "multiple-clients",
+        accept: {
+          "text/plain": [".txt", ".md"],
+        },
+        icons: [android192],
+      },
+    ],
+  };
+}
